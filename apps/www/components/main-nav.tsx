9be12a1f6da9a1c6aca0c56eb1dbadf9b5e3caa2
pavincius <30844451+PAVincius@users.noreply.github.com>
2023-11-10 18:15:49 -0300

    feat(core): add fixed v1.2 galaxies

--- apps/www/components/main-nav.tsx
+++ apps/www/components/main-nav.tsx
@@ -16,7 +16,7 @@ export function MainNav() {
     <div className="mr-4 hidden md:flex">
       <Link href="/" className="mr-6 flex items-center space-x-2">
         <Icons.logo className="h-6 w-6" />
-        <span className="hidden font-bold sm:inline-block">
+        <span className="hover:text-primary hidden font-bold sm:inline-block">
           {siteConfig.name}
         </span>
       </Link>
@@ -24,7 +24,7 @@ export function MainNav() {
         <Link
           href="/docs"
           className={cn(
-            "transition-colors hover:text-foreground/80",
+            "hover:text-foreground/80 transition-colors",
             pathname === "/docs" ? "text-foreground" : "text-foreground/60"
           )}
         >
@@ -33,7 +33,7 @@ export function MainNav() {
         <Link
           href="/docs/components"
           className={cn(
-            "transition-colors hover:text-foreground/80",
+            "hover:text-foreground/80 transition-colors",
             pathname?.startsWith("/docs/components")
               ? "text-foreground"
               : "text-foreground/60"
@@ -44,7 +44,7 @@ export function MainNav() {
         <Link
           href="/themes"
           className={cn(
-            "transition-colors hover:text-foreground/80",
+            "hover:text-foreground/80 transition-colors",
             pathname?.startsWith("/themes")
               ? "text-foreground"
               : "text-foreground/60"
@@ -55,7 +55,7 @@ export function MainNav() {
         <Link
           href="/examples"
           className={cn(
-            "transition-colors hover:text-foreground/80",
+            "hover:text-foreground/80 transition-colors",
             pathname?.startsWith("/examples")
               ? "text-foreground"
               : "text-foreground/60"
@@ -66,7 +66,7 @@ export function MainNav() {
         <Link
           href={siteConfig.links.github}
           className={cn(
-            "hidden text-foreground/60 transition-colors hover:text-foreground/80 lg:block"
+            "text-foreground/60 hover:text-foreground/80 hidden transition-colors lg:block"
           )}
         >
           GitHub
--- apps/www/config/site.ts
+++ apps/www/config/site.ts
@@ -1,12 +1,12 @@
 export const siteConfig = {
-  name: "shadcn/ui",
-  url: "https://ui.shadcn.com",
+  name: "galaxies/ui",
+  url: "https://galaxies.nebuloz.com",
   ogImage: "https://ui.shadcn.com/og.jpg",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   links: {
     twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn-ui/ui",
+    github: "https://github.com/Nebuloz/Galaxies",
   },
 }
 
